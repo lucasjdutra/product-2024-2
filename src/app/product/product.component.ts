0c9ac20ec12c5bdda7576dec9923314d2026a0d8
lucasjdutra <lucasjdc90@gmail.com>
2024-08-27 11:09:09 -0300

    dynamic table with objects

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-
+    products: Product[] = [
+      { "id": 1,
+        "name": "Celtinha 2024",
+        "price": 1.99,
+        "category": "Carro"
+      },
+      { "id": 2,
+        "name": "Notebook Positivo no precinho",
+        "price": 1000000000000.00,
+        "category": "PC"
+      },
+      { "id": 3,
+        "name": "Cachorro de Calça",
+        "price": 0.00,
+        "category": "Cachorro (de calça)"
+      }
+    ];
 }
